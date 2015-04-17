0efa4f05a890a12e1dcaef1bd31b9dc8ae390f33
Albertus82 <albertus82@gmail.com>
2015-04-17 22:26:26 +0200

    SpringMvcBasic

---
+++ SpringMvcBasic/src/it/albertus/spring/config/RootConfig.java
@@ -0,0 +1,16 @@
+package it.albertus.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = { "it.albertus.spring" },
+	excludeFilters = {
+		@Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
+	})
+public class RootConfig {
+
+}
