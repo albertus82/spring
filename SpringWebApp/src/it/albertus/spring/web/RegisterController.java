170c0acf195e5068f413910df0272fc1073dc6d4
Albertus82 <albertus82@gmail.com>
2015-04-20 20:39:24 +0200

    @ExceptionHandler

--- SpringWebApp/src/it/albertus/spring/web/RegisterController.java
+++ SpringWebApp/src/it/albertus/spring/web/RegisterController.java
@@ -13,13 +13,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RegisterController {
-
+	
+	@Autowired
+	private UtenteService utenteService;
+	
 	@ModelAttribute("utente")
 	private Utente createUtente() {
 		Utente utente = new Utente();
@@ -27,16 +31,18 @@ public class RegisterController {
 		return utente;
 	}
 	
-	@Autowired
-	private UtenteService utenteService;
-
+	@ExceptionHandler(Exception.class)
+	public String handleException(Exception e, HttpServletRequest request) {
+		return "redirect:error?page=" + this.getClass().getSimpleName();
+	}
+	
 	@RequestMapping(value = { "/register" }, method = RequestMethod.GET)
 	public String register(Utente utente, Errors errori) {
 		return "register"; // Forward!
 	}
 
 	@RequestMapping(value = { "/register" }, method = RequestMethod.POST)
-	public String register(@Valid Utente utente, Errors errori, BindingResult result, HttpServletRequest request, Model model) {
+	public String register(@Valid Utente utente, Errors errori, BindingResult result, HttpServletRequest request, Model model) throws Exception {
 		String forward;
 		if (errori.hasErrors()) {
 			model.addAttribute("errori", errori.getFieldErrors());
@@ -48,8 +54,9 @@ public class RegisterController {
 			forward = "redirect:login?messaggio={messaggio}";
 		}
 		catch (Exception e) {
-			request.setAttribute("messaggio", "Errore di registrazione: " + e.getLocalizedMessage());
-			forward = register(utente, errori);
+			throw e;
+//			request.setAttribute("messaggio", "Errore di registrazione: " + e.getLocalizedMessage());
+//			forward = register(utente, errori);
 		}
 		return forward;
 	}
