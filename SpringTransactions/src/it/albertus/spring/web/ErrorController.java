cc45168908bd5ac2e1673583bc2afcf8b959191d
Albertus82 <albertus82@gmail.com>
2015-04-23 16:34:54 +0200

    SpringTransactions (da completare).

---
+++ SpringTransactions/src/it/albertus/spring/web/ErrorController.java
@@ -0,0 +1,15 @@
+package it.albertus.spring.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ErrorController {
+	
+	@RequestMapping(value = { "/error" }, method = RequestMethod.GET)
+	public String error() {
+		return "error"; // Forward!
+	}
+
+}
