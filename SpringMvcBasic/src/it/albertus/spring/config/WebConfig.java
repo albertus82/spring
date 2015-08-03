827dc7b1e28dfc91716b0f9f5c19d036d03c22a9
Albertus82 <albertus82@gmail.com>
2015-08-03 11:50:17 +0200

    UrlBasedViewResolver.

--- SpringMvcBasic/src/it/albertus/spring/config/WebConfig.java
+++ SpringMvcBasic/src/it/albertus/spring/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.DefaultServletHandlerCo
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -17,7 +18,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	/** Configurazione del resolver per le JSP */
 	@Bean
 	public ViewResolver viewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		UrlBasedViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		resolver.setExposeContextBeansAsAttributes(true);
