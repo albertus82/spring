2bcb4ba337f614392d1870b1bce24971e10e41ff
Albertus82 <albertus82@gmail.com>
2015-08-18 17:56:21 +0200

    @Required.

--- SpringCoreBasic/src/main/java/it/albertus/spring/service/AccountServiceImpl.java
+++ SpringCoreBasic/src/main/java/it/albertus/spring/service/AccountServiceImpl.java
@@ -2,10 +2,13 @@ package it.albertus.spring.service;
 
 import it.albertus.spring.dao.AccountDao;
 
+import org.springframework.beans.factory.annotation.Required;
+
 public class AccountServiceImpl implements AccountService {
 
 	private AccountDao accountDao;
 
+	@Required
 	public void setAccountDao(AccountDao accountDao) {
 		this.accountDao = accountDao;
 	}
