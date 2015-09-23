249e5f58cb0b29fd2c01f5014b7c85bf9d6dbd66
Albertus82 <albertus82@gmail.com>
2015-09-23 22:38:56 +0200

    Commento allo script SQL.

--- SpringJTA/sql/01_GRANT.sql
+++ SpringJTA/sql/01_GRANT.sql
@@ -1,3 +1,4 @@
+/* Oracle resources can only be recovered when the configured user has these privileges: */
 GRANT SELECT ON sys.dba_pending_transactions TO alb_admin;
 GRANT SELECT ON sys.pending_trans$ TO alb_admin;
 GRANT SELECT ON sys.dba_2pc_pending TO alb_admin;
