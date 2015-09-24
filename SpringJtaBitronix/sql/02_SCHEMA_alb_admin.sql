eebc5b6bccf5c65f79ca9ed3a2dd77e16099a415
Albertus82 <albertus82@gmail.com>
2015-09-24 22:44:11 +0200

    Correzione script SQL.

--- SpringJtaBitronix/sql/02_SCHEMA_alb_admin.sql
+++ SpringJtaBitronix/sql/02_SCHEMA_alb_admin.sql
@@ -6,10 +6,9 @@ GRANT EXECUTE ON sys.DBMS_SYSTEM TO alb_admin;
 
 CREATE TABLE alb_admin.utenti (
     username VARCHAR2 (50 CHAR) CONSTRAINT pk_utenti PRIMARY KEY,
-    password VARCHAR2 (50 CHAR) CONSTRAINT nn_utenti_password NOT NULL,
-    nome VARCHAR2 (50 CHAR) CONSTRAINT nn_utenti_nome NOT NULL,
-    cognome VARCHAR2 (50 CHAR) CONSTRAINT nn_utenti_cognome NOT NULL,
+    password VARCHAR2 (50 CHAR) CONSTRAINT nn1_utenti_password NOT NULL,
+    nome VARCHAR2 (50 CHAR) CONSTRAINT nn2_utenti_nome NOT NULL,
+    cognome VARCHAR2 (50 CHAR) CONSTRAINT nn3_utenti_cognome NOT NULL,
     data_nascita DATE,
-    data_inserimento TIMESTAMP (3)
-            DEFAULT SYSTIMESTAMP
-            CONSTRAINT nn_utenti_data_inserimento NOT NULL);
+    data_inserimento TIMESTAMP (3) DEFAULT SYSTIMESTAMP CONSTRAINT nn4_utenti_data_inserimento NOT NULL
+);
