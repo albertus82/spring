ce1bec7dee605dedd71fc72d93dc7ddc96e1deab
Alberto <Albertus82@users.noreply.github.com>
2015-09-24 22:29:05 +0200

    Update 01_SCHEMA_hr.sql

--- SpringJtaBitronix/sql/01_SCHEMA_hr.sql
+++ SpringJtaBitronix/sql/01_SCHEMA_hr.sql
@@ -8,7 +8,7 @@ CREATE TABLE hr.jta_bitronix_test
 (
     id                 NUMBER (20, 0) CONSTRAINT pk_jta_bitronix_test PRIMARY KEY,
     testo              VARCHAR2 (64 CHAR) CONSTRAINT nn_jta_bitronix_test_text NOT NULL,
-    data_inserimento   TIMESTAMP (3) DEFAULT SYSTIMESTAMP CONSTRAINT nn_jta_bitronix_text_data_ins NOT NULL
+    data_inserimento   TIMESTAMP (3) DEFAULT SYSTIMESTAMP CONSTRAINT nn_jta_bitronix_test_data_ins NOT NULL
 );
 
 CREATE SEQUENCE hr.seq_jta_bitronix_test;
