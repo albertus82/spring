045394f72a35ad168bbc30937350e7e71ffaa48a
Albertus82 <albertus82@gmail.com>
2016-01-23 08:45:08 +0100

    Storage Client (Azure).

---
+++ SpringStorageClient/src/main/java/it/albertus/storage/StorageClientLauncher.java
@@ -0,0 +1,39 @@
+package it.albertus.storage;
+
+import it.albertus.storage.config.SpringContext;
+
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class StorageClientLauncher {
+
+	public static void main(String[] args) {
+		if (args.length != 2 || args[0].trim().length() == 0 || args[1].trim().length() == 0) {
+			throw new IllegalArgumentException("Parametri obbligatori: nomeFileSorgente percorsoDestinazione.");
+		}
+
+		final String source = args[0];
+		final String destination = args[1] + '/' + args[0];
+
+		ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(SpringContext.class);
+
+		FileStorage client = context.getBean(FileStorage.class);
+
+		try {
+			client.downloadToFile(source, destination);
+			if (source.endsWith(".txt")) {
+				System.out.println("Contenuto del file " + source + ": \"" + IOUtils.toString(client.downloadAsStream(source)) + '"');
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			context.close();
+		}
+	}
+
+}
