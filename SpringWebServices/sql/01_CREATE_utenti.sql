1c7bf1e12bf8c02cf76eccfa335b07c08f2309f0
Albertus82 <albertus82@gmail.com>
2016-01-27 12:58:51 +0100

    Rimosso progetto SpringWebServices.

--- SpringWebServices/sql/01_CREATE_utenti.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE utenti
-(
-    username       VARCHAR2 (50 CHAR) CONSTRAINT pk_utenti PRIMARY KEY,
-    password       VARCHAR2 (50 CHAR) CONSTRAINT nn_utenti_password NOT NULL,
-    nome           VARCHAR2 (50 CHAR) CONSTRAINT nn_utenti_nome NOT NULL,
-    cognome        VARCHAR2 (50 CHAR) CONSTRAINT nn_utenti_cognome NOT NULL,
-    data_nascita   DATE
-);
