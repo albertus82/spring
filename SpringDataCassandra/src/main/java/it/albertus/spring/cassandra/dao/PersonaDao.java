6e4d4990a9516c444305e3f34122e2f91d2477bb
Albertus82 <albertus82@gmail.com>
2016-01-27 06:13:38 +0100

    Corretto tipo chiave.

--- SpringDataCassandra/src/main/java/it/albertus/spring/cassandra/dao/PersonaDao.java
+++ SpringDataCassandra/src/main/java/it/albertus/spring/cassandra/dao/PersonaDao.java
@@ -2,10 +2,12 @@ package it.albertus.spring.cassandra.dao;
 
 import it.albertus.spring.cassandra.model.Persona;
 
+import java.util.UUID;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonaDao extends CrudRepository<Persona, Integer> {
+public interface PersonaDao extends CrudRepository<Persona, UUID> {
 
 }
